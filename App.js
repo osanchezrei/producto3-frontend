f95ff5e097830db52f80416bffc21fb75cf55438
oriolSanchez <ori.reina@gmail.com>
2022-12-04 12:02:49 +0100

    Componentes creados

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Jugadores } from './app/views/Jugadores.js'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Jugadores></Jugadores>
       <StatusBar style="auto" />
     </View>
   );
